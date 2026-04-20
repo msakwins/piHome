15527442d592492263b33f2828e4f22079547488
msakwins <marion.sakwi@gmail.com>
2026-04-20 11:12:26 +0200

    added token in vercel

--- src/components/getNextTrain.js
+++ src/components/getNextTrain.js
@@ -1,25 +1,10 @@
-// API KEY: Oiy3tBJUI6aRR9fdLEBSYYY7CjgHaOYK
-
-const apiKey = 'Oiy3tBJUI6aRR9fdLEBSYYY7CjgHaOYK';
-const stopId = '44493'; // Bagneux
-
 export const getNextTrain = async () => {
-    const monitoringRef = `STIF:StopArea:SP:${stopId}:`;
-    
-    const url = `https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=${monitoringRef}`;
-
-    try {
-        const response = await fetch(url, {
-            headers: { 'apikey': apiKey }
-        });
-        const data = await response.json();
-        
-        // Extraction
-        const visits = data?.Siri?.ServiceDelivery?.StopMonitoringDelivery?.[0]?.MonitoredStopVisit;
-        
-        return visits || []; 
-    } catch (error) {
-        console.error('Error fetching IDFM data:', error);
-        return [];
-    }
+  try {
+    const response = await fetch('/api/idfm')
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    return []
+  }
 }
--- src/hooks/useDayNight.js
+++ src/hooks/useDayNight.js
@@ -1,34 +1,36 @@
 import { useEffect, useState } from "react"
-// OpenWeather API key fd77b9334ee8f53a0612a6117d8fb1d7
-
-// const ApiKey = "fd77b9334ee8f53a0612a6117d8fb1d7";
 
 export function useDayNight(lat, lon) {
   const [isNight, setIsNight] = useState(false)
 
-  useEffect(() => {
-    async function fetchSun() {
-      const res = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=sunrise,sunset&timezone=auto`
-      )
-      const data = await res.json()
-      console.log("weather:", await fetch(
-              `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m,precipitation&daily=sunrise,sunset&timezone=Europe/Paris`
+useEffect(() => {
+  async function fetchSun() {
+    const res = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=sunrise,sunset&timezone=auto`
+    )
 
-      ));
+    const data = await res.json()
 
-      const sunrise = new Date(data.daily.sunrise[0])
-      const sunset = new Date(data.daily.sunset[0])
-      const now = new Date()
+    const weatherRes = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m,precipitation&daily=sunrise,sunset&timezone=Europe/Paris`
+    )
 
-      setIsNight(now < sunrise || now > sunset)
-    }
+    const weatherData = await weatherRes.json()
 
-    fetchSun()
-    const interval = setInterval(fetchSun, 600000) // refresh toutes les 10 min
+    console.log("weather:", weatherData)
 
-    return () => clearInterval(interval)
-  }, [lat, lon])
+    const sunrise = new Date(data.daily.sunrise[0])
+    const sunset = new Date(data.daily.sunset[0])
+    const now = new Date()
+
+    setIsNight(now < sunrise || now > sunset)
+  }
+
+  fetchSun()
+  const interval = setInterval(fetchSun, 600000)
+
+  return () => clearInterval(interval)
+}, [lat, lon])
 
   return isNight
 }
